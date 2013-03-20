a9c38687b0355494c2b51be9340c1b89fad329d6
william <william@william-1015BX-1015BX.(none)>
2013-03-20 17:42:20 -0300

    gg

--- apps/root/server.js
+++ apps/root/server.js
@@ -1,1 +1,17 @@
-console.log('starting nodeJS..............');
+var express = require('express');
+var app = express();
+
+app.get('/hello.txt', function(req, res){
+  var body = 'Hello World';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
+});
+
+
+app.get('/hello.txt', function(req, res){
+  res.send('Hello World');
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
